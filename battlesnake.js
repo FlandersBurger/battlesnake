5aa2c618e9148cae7d92c5ac61839adb19399844
unknown <debackerlaurent@gmail.com>
2020-03-06 23:16:33 -0800

    Pick direction based on body

--- battlesnake.js
+++ battlesnake.js
@@ -54,19 +54,23 @@ router.post('/move', function ({ body }, res, next) {
     if (bestDirections.indexOf(games[body.game.id]) >= 0) {
       direction = games[body.game.id];
     } else if (bestDirections.length > 0 && bestDirections.indexOf(games[body.game.id]) < 0) {
-      direction = bestDirections[Math.floor(Math.random() * bestDirections.length)];
+      //direction = bestDirections[Math.floor(Math.random() * bestDirections.length)];
+      direction = pickDirection(bestDirections, body.you);
     } else if (validDirections.indexOf(games[body.game.id]) >= 0) {
       direction = games[body.game.id];
     } else {
-      direction = validDirections[Math.floor(Math.random() * validDirections.length)];
+      //direction = validDirections[Math.floor(Math.random() * validDirections.length)];
+      direction = pickDirection(validDirections, body.you);
     }
   } else {
     const random = Math.random() * 100;
     console.log(`Rando! ${random}`);
     if (random < body.you.health || bestDirections.length === 0) {
-      direction = validDirections[Math.floor(Math.random() * validDirections.length)];
+      //direction = validDirections[Math.floor(Math.random() * validDirections.length)];
+      direction = pickDirection(validDirections, body.you);
     } else {
-      direction = bestDirections[Math.floor(Math.random() * bestDirections.length)];
+      //direction = bestDirections[Math.floor(Math.random() * bestDirections.length)];
+      direction = pickDirection(bestDirections, body.you);
     }
   }
 
@@ -93,6 +97,23 @@ router.get('/', function (req, res, next) {
 
 const distance = (spot1, spot2) => {
   return Math.abs(spot1.x - spot2.x) + Math.abs(spot1.y - spot2.y)
+};
+
+const pickDirection = (directions, me) => {
+  const head = me.body[0];
+  const pieces = {
+    'up': me.body.slice(1, me.body.length).reduce((sum, piece) => sum += piece.y < head.y ? 1 : 0, 0),
+    'down': me.body.slice(1, me.body.length).reduce((sum, piece) => sum += piece.y > head.y ? 1 : 0, 0),
+    'left': me.body.slice(1, me.body.length).reduce((sum, piece) => sum += piece.x < head.x ? 1 : 0, 0),
+    'right': me.body.slice(1, me.body.length).reduce((sum, piece) => sum += piece.x > head.x ? 1 : 0, 0)
+  };
+  return directions.reduce((chosenDirection, direction) => {
+    if (pieces[direction] < chosenDirection.pieces) {
+      chosenDirection.direction = direction;
+      chosenDirection.pieces = pieces[direction];
+    }
+  }, { direction: '', pieces: 1000 }).direction;
+
 };
 
 const checkSpot = (board, snakes, me, position) => {
