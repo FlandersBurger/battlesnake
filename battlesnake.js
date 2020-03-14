bee3dccf74aac63512133c1a77787409c2453684
unknown <debackerlaurent@gmail.com>
2020-03-13 21:45:33 -0700

    Logs on what the snake decides

--- battlesnake.js
+++ battlesnake.js
@@ -57,7 +57,7 @@ router.post("/move", function({ body }, res, next) {
   }
   console.log(directions);
   //const validDirections = directions.filter(direction => direction.score > -5).map(direction => direction.direction);
-  const bestDirection = directions.reduce((best, direction) => {
+  const highScoreDirection = directions.reduce((best, direction) => {
     if (best.score < direction.score) {
       best = direction;
     }
@@ -67,17 +67,22 @@ router.post("/move", function({ body }, res, next) {
   let direction;
   const bestDirections = _.intersection(validDirections, foodDirections);
   if (bestDirections.indexOf(games[body.game.id]) >= 0) {
+    console.log('Best Directions include Previous Direction');
+    direction = games[body.game.id];
+  } else if (bestDirections.indexOf(highScoreDirection.direction) >= 0) {
+    console.log('Best Directions include High Score Direction');
+    direction = highScoreDirection.direction;
+  } else if (validDirections.indexOf(highScoreDirection.direction) >= 0) {
+    console.log('Valid Directions includes High Score Direction');
+    direction = highScoreDirection.direction;
+  } else if (validDirections.indexOf(games[body.game.id]) >= 0) {
+    console.log('Valid Directions includes Previous Direction');
     direction = games[body.game.id];
   } else if (bestDirections.length > 0 && bestDirections.indexOf(games[body.game.id]) < 0) {
-    //direction = bestDirections[Math.floor(Math.random() * bestDirections.length)];
+    console.log('There are Best Directions, pick one');
     direction = pickDirection(bestDirections, body.you, body.board);
-  } else if (validDirections.indexOf(bestDirection.direction) >= 0) {
-    direction = bestDirection.direction;
-  } else if (validDirections.indexOf(games[body.game.id]) >= 0) {
-    direction = games[body.game.id];
-  } else if (foodDirections.indexOf(bestDirection.direction) >= 0 || bestDirection.direction) {
-    direction = bestDirection.direction;
   } else {
+    console.log('There are Valid Directions, pick one');
     //direction = validDirections[Math.floor(Math.random() * validDirections.length)];
     direction = pickDirection(validDirections, body.you, body.board);
   }
